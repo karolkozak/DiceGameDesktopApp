d45d032493911c67b95615fba002cc2bc4a4cf84
Jakub <jakub.makselon@gmail.com>
2017-01-03 12:40:06 +0100

    dodanie wining number i points to win go gamestate

--- src/com/dicegame/model/GameState.java
+++ src/com/dicegame/model/GameState.java
@@ -10,9 +10,15 @@ public class GameState {
     private List<Player> listOfPlayers;
     private GameStatus status;
     private String activePlayer;
+    private int winningNumber;
+    private int numberOfPointsToWin;
 
-    public GameState(List<Player> listOfPlayers, GameStatus status) {
+
+    public GameState(List<Player> listOfPlayers, GameStatus status, String activePlayer, int winningNumber, int numberOfPointsToWin) {
         this.listOfPlayers = listOfPlayers;
         this.status = status;
+        this.activePlayer = activePlayer;
+        this.winningNumber = winningNumber;
+        this.numberOfPointsToWin = numberOfPointsToWin;
     }
 }
