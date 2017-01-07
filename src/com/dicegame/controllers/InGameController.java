fd327415e9ed43b93fff49fe5d9a465589d3547b
karolkozak <karol2868@gmail.com>
2017-01-07 23:40:09 +0100

    Add, Update, Improve controllers

---
+++ src/com/dicegame/controllers/InGameController.java
@@ -0,0 +1,26 @@
+package com.dicegame.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by Karol on 07/01/2017.
+ */
+public class InGameController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    public void handleSurrenderAction(ActionEvent actionEvent) {
+        
+    }
+
+    public void handleRollAction(ActionEvent actionEvent) {
+
+    }
+}
