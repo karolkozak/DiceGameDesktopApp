7bde5d5c4b201cf82206c576a5859cb4d0f3ef6d
Jakub <jakub.makselon@gmail.com>
2017-01-18 15:41:21 +0100

    comment test

--- src/main/java/com/dicegame/tests/Receiver.java
+++ src/main/java/com/dicegame/tests/Receiver.java
@@ -1,16 +1,16 @@
-package com.dicegame.tests;
-
-import org.springframework.jms.annotation.JmsListener;
-import org.springframework.stereotype.Component;
-
-/**
- * Created by Karol on 09/01/2017.
- */
-//@Component
-public class Receiver {
-
-   // @JmsListener(destination = "mailbox", containerFactory = "myFactory")
-    public void receiveMessage(String email) {
-        System.out.println("Received <" + email + ">");
-    }
-}
+//package com.dicegame.tests;
+//
+//import org.springframework.jms.annotation.JmsListener;
+//import org.springframework.stereotype.Component;
+//
+///**
+// * Created by Karol on 09/01/2017.
+// */
+////@Component
+//public class Receiver {
+//
+//   // @JmsListener(destination = "mailbox", containerFactory = "myFactory")
+//    public void receiveMessage(String email) {
+//        System.out.println("Received <" + email + ">");
+//    }
+//}
